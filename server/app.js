2fb952ab054c5c9b1ddbcf1ea60698eefb4a8452
gotik <dark@shamain.com.ar>
2012-08-06 22:16:12 -0500

    better landspace

--- server/app.js
+++ server/app.js
@@ -1,19 +1,17 @@
-var
-    id = 0
-    , instances = {}
-    , instances_tmp = {}
-;
-
 var 
     http = require('http')
 
-    , app = http.createServer(function (request, response) {
-        response.writeHead(200)
-        response.end()
+    , app = http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('caju\n');
     })
 
     , port = process.env.PORT || 5000
     , io = require('socket.io').listen(app)
+
+    , id = 0
+    , instances = {}
+    , instances_tmp = {}
 ;
 
 app.listen(port);
@@ -26,11 +24,6 @@ io.sockets.on('connection', function (socket) {
         instances_tmp[id] = [socket, player];
         socket.emit('new', {id: id});
         id++;
-
-        /*console.log(socket);
-        instances[id] = [socket, null, mapUrl];
-        socket.emit('new', {id: id, mapUrl: mapUrl});
-        id++;*/
     });
 
     socket.on('join', function (data) {
@@ -39,24 +32,15 @@ io.sockets.on('connection', function (socket) {
 
         var socketAFull = instances_tmp[instanceId];
 
-        // Conectados los sockets :)
-
+        // Players connected PvP
         if(socketAFull) {
             var socketA = socketAFull[0];
-
             var playerA = socketAFull[1];
-
-            //console.log(socketB.id, socket.id)
 
             delete instances_tmp[instanceId];
 
             instances[socketA.id] = socket;
             instances[socket.id] = socketA;
-
-
-            /*for(var key in instances) {
-                console.log(key, instances[key].id);
-            }*/
 
             socket.emit('ready', playerA);
             socketA.emit('ready', playerB);
@@ -64,15 +48,6 @@ io.sockets.on('connection', function (socket) {
         } else {
             socket.emit('join', false);
         }
-        /*console.log(io.sockets)
-        if(instances[instanceId] && instances[instanceId][1] == null && instances[instanceId][0] != socket) {
-            // Set the two players at the same room
-            instances[instanceId][1] = socket;
-            // Send to the second player the map url to load
-            socket.emit('join', true);
-        } else {
-            socket.emit('join', false);
-        }*/
     });
 
     socket.on('ready', function (instanceId) {
@@ -81,25 +56,19 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('move', function (data) {
-        //console.log(data.id, data.py);
-        //if(instances[data.id][data.py]) instances[data.id][data.py].emit('move', data.data);
-        instances[socket.id].emit('move', data.data);
+        if(instances[socket.id]) {
+            instances[socket.id].emit('move', data.data);
+        }
     });
 
-    /*socket.on('data', function (data) {
-
-    });*/
-
     socket.on('disconnect', function () {
-        //TODO
         var socketB = instances[socket.id];
 
         if(socketB) {
             socketB.emit('bye');
-
             delete instances[socket.id];
             delete instances[socketB.id];
         }
     });
 
-})
+});
